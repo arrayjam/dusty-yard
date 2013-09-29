bcf1f1090e2148d79bad5ce60dc2c9cb11c1b6be
Yuri Feldman <arrayjam@tbqh.net>
2013-09-29 16:59:25 +1000

    Convert to node

--- dusty-yard.js
+++ dusty-yard.js
@@ -1,33 +1,20 @@
+var d3 = require("d3"),
+  fs = require("fs"),
+  queue = require("queue-async"),
+  topojson = require("topojson");
+
 queue()
-  .defer(d3.json, "sa1.json")
-  .defer(d3.csv, "booths.csv")
+  .defer(fs.readFile, __dirname + "/data/sa1.json", { encoding: "utf-8" })
+  .defer(fs.readFile, __dirname + "/data/booths.csv", { encoding: "utf-8" })
   .await(ready);
 
+// voronoi -> clustered booths + long/lap point + sa1 codes
+
 function ready(error, sa1, boothdata) {
-  //var path = d3.geo.path()
-      //.projection(null);
+  sa1 = JSON.parse(sa1);
+  boothdata = d3.csv.parse(boothdata);
 
-
-  //var width = 960,
-    //height = 500;
-
-  //var ausCenter = [132.5, -26.5];
-  //var parallels = [-36, -18];
-
-  //var projection = d3.geo.albers()
-    //.translate([width / 2, height / 2])
-    //.scale(1100)
-    //.rotate([-ausCenter[0], 0])
-    //.center([0, ausCenter[1]])
-    //.parallels(parallels)
-    //.precision(0);
-
-  //var centroids = [];
   var geo = topojson.feature(sa1, sa1.objects.sa1).features;
-
-  //geo.forEach(function(feature) {
-    //centroids.push({id: feature.id, coords: path.centroid(feature)});
-  //});
 
   boothdata.forEach(function(booth) {
     booth.Longitude = +booth.Longitude;
@@ -42,24 +29,24 @@ function ready(error, sa1, boothdata) {
   threshold = 0.0007;
   boothdata.forEach(function(booth) {
     var c = false;
-    clustered.forEach(function(clustered) {
-      if (Math.abs(booth.Longitude - clustered.Longitude) <= threshold &&
-          Math.abs(booth.Latitude - clustered.Latitude) <= threshold) {
+    for (var i = 0; i < clustered.length; i++) {
+      var cluster = clustered[i];
+      if (Math.abs(booth.Longitude - cluster.Longitude) <= threshold &&
+          Math.abs(booth.Latitude - cluster.Latitude) <= threshold) {
         c = true;
-        clustered.cluster.push(booth);
+      cluster.cluster.push(booth);
+      break;
       }
-    });
+    }
     if (!c) {
       clustered.push(booth);
     }
   });
 
-
-  //var projected = clustered.map(function(d) { return d.projected;});
   var polygons = d3.geom.voronoi()
-    .x(function(d) { return d.Longitude; })
-    .y(function(d) { return d.Latitude; })
-    (clustered);
+  .x(function(d) { return d.Longitude; })
+  .y(function(d) { return d.Latitude; })
+  (clustered);
 
   var centroids = d3.map();
   geo.forEach(function(d) {
@@ -78,79 +65,24 @@ function ready(error, sa1, boothdata) {
     });
   });
 
-  window.result = result;
+  var geojson = {
+    "type": "FeatureCollection",
+    "features": []
+  };
 
+  var feature = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Polygon",
+      "coordinates": [[
+        [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]
+      ]]
+    },
+    "properties": { "prop0": "value0" },
+    "id": "xxxx"
+  };
 
-  window.c = clustered;
-  window.geo = geo;
-  window.centroids = centroids;
-  window.polygons = polygons;
-  window.boothdata = boothdata;
-
-  console.log("clustered", clustered);
-  console.log("geo", geo);
-  console.log("centroids", centroids);
-
-  console.log("polygons", polygons);
-
-  clustered.forEach(function(cluster) {
-    //console.log(centroids.get(cluster.PollingPlaceID));
-    //console.log(cluster);
-  });
-
-  //function pointInPolygon (point, vs) {
-
-  //var svg = d3.select("body").append("svg")
-    //.attr("width", width)
-    //.attr("height", height);
-
-  //var path = d3.geo.path()
-    //.projection(projection);
-
-  //var g = svg.append("g");
-
-  //g.selectAll("path.land")
-    //.data(geo)
-    //.enter().append("path")
-    //.attr("class", "land")
-    //.attr("d", path);
-
-  //g.selectAll("path.voronoi")
-    //.data(polygons)
-    //.enter().append("path")
-    //.attr("class", "voronoi")
-    //.attr("d", function(d) { return "M" + d.join("L") + "Z"; });
-  //g.selectAll("circle.booth")
-    //.data(clustered)
-    //.enter().append("circle")
-    //.attr("class", "booth")
-    //.attr("r", 1)
-    //.attr("cx", function(d) { return d.projected[0]; })
-    //.attr("cy", function(d) { return d.projected[1]; });
-
-
-  // zoom and pan
-  //var zoom = d3.behavior.zoom()
-  //.on("zoom",function() {
-    //g.attr("transform","translate("+
-           //d3.event.translate.join(",")+")scale("+d3.event.scale+")");
-    //g.selectAll("path.land")
-      //.attr("d", path.projection(projection))
-      //.style("stroke-width", 1 / d3.event.scale);
-    //g.selectAll("path.voronoi")
-      //.attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-      //.style("stroke-width", 1 / d3.event.scale);
-
-    //g.selectAll("circle.booth")
-      //.attr("r", 1 / d3.event.scale);
-
-  //});
-
-  //svg.call(zoom);
-
-
-//    //window.booths[+booth.PollingPlaceID] = [+booth.Longitude, +booth.Latitude];
-
+  fs.writeFile("result.json", JSON.stringify(result));
 }
 
 function pointInPolygon (point, vs) {

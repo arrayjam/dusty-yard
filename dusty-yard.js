067e3872b1eb8736efee4ee4e9119a17b31ee2ef
Yuri Feldman <arrayjam@tbqh.net>
2013-10-01 23:16:12 +1000

    Clip voronoi and generate geojson points and polygons

--- dusty-yard.js
+++ dusty-yard.js
@@ -1,6 +1,6 @@
 var d3 = require("d3"),
-  fs = require("fs"),
   queue = require("queue-async"),
+  fs = require("fs"),
   topojson = require("topojson"),
   sphereKnn = require("sphere-knn");
 
@@ -13,13 +13,16 @@ queue()
 //booth.projected = projection([booth.Longitude, booth.Latitude]);
 
 // In meters
-var clusteringThreshold = 500;
+var clusteringThreshold = 1;
 
 function ready(error, sa1, boothdata) {
   sa1 = JSON.parse(sa1);
   boothdata = d3.csv.parse(boothdata);
 
   var geo = topojson.feature(sa1, sa1.objects.sa1).features;
+  var xExtent = d3.extent(geo, function(d) { return (d.geometry.coordinates[0][0][0]); });
+  var yExtent = d3.extent(geo, function(d) { return (d.geometry.coordinates[0][0][1]); });
+  var extent = [[ xExtent[0] - 10, yExtent[0] - 10 ], [ xExtent[1] + 20, yExtent[1] + 10 ]];
 
   boothdata = boothdata.map(function(booth) {
     return {
@@ -55,6 +58,7 @@ function ready(error, sa1, boothdata) {
   var polygons = d3.geom.voronoi()
     .x(function(d) { return d.position[0]; })
     .y(function(d) { return d.position[1]; })
+    .clipExtent(extent)
     (clustered.values());
 
   // Transform the polygons into a map
@@ -88,23 +92,17 @@ function ready(error, sa1, boothdata) {
     });
   });
 
-  var geojson = {
-    "type": "FeatureCollection",
-    "features": []
-  };
+  var voronoi = geojson();
 
   result.values().forEach(function(result) {
-    var feature = polygonFeature(result.id, {
+    voronoi.features.push(polygonFeature(result.id, {
       booths: result.booths,
       tracts: result.tracts
-    }, result.voronoi);
-    geojson.features.push(feature);
-    console.log(feature);
-
+    }, result.voronoi));
   });
 
   fs.writeFile("data/result.json", JSON.stringify(result, null, 2));
-  fs.writeFile("data/geo.json", JSON.stringify(geojson));
+  fs.writeFile("data/voronoi.json", JSON.stringify(voronoi, null, 2));
 }
 
 function polygonFeature (id, properties, coordinates) {
@@ -119,6 +117,31 @@ function polygonFeature (id, properties, coordinates) {
   };
 }
 
+function pointFeature (id, coordinates) {
+  return {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": coordinates
+    },
+    "properties": {},
+    "id": id
+  };
+}
+
+function geojson() {
+  return {
+    "type": "FeatureCollection",
+    "features": [],
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+      }
+    }
+  };
+}
+
 function search(quadtree, x0, y0, x3, y3, callback) {
   quadtree.visit(function(node, x1, y1, x2, y2) {
     var p = node.point;
@@ -126,6 +149,7 @@ function search(quadtree, x0, y0, x3, y3, callback) {
     return x1 >= x3 || y1 >= y3 || x2 < x0 || y2 < y0;
   });
 }
+
 function pointInPolygon (point, vs) {
   // ray-casting algorithm based on
   // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html

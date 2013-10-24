4972fc0a004bc4b49746d9625725544b81425bae
Yuri Feldman <arrayjam@tbqh.net>
2013-10-24 19:23:52 +1100

    Collate tract data into booths. Woo!

--- valanalys.js
+++ valanalys.js
@@ -8,6 +8,7 @@ queue()
   .defer(fs.readFile, __dirname + "/data/sa1.json", { encoding: "utf-8" })
   .defer(fs.readFile, __dirname + "/data/booths.csv", { encoding: "utf-8" })
   .defer(fs.readFile, __dirname + "/data/votes.csv", { encoding: "utf-8" })
+  .defer(fs.readFile, __dirname + "/sa1.csv", { encoding: "utf-8" })
   .await(ready);
 
 // voronoi -> clustered booths + long/lap point + sa1 codes
@@ -16,10 +17,11 @@ queue()
 // In meters
 var clusteringThreshold = 1000;
 
-function ready(error, sa1, boothdata, votes) {
+function ready(error, sa1, boothdata, votes, sa1data) {
   sa1 = JSON.parse(sa1);
   boothdata = d3.csv.parse(boothdata);
   votes = d3.csv.parse(votes);
+  sa1data = d3.csv.parse(sa1data);
 
   var geo = topojson.feature(sa1, sa1.objects.sa1).features;
   var xExtent = d3.extent(geo, function(d) { return (d.geometry.coordinates[0][0][0]); });
@@ -85,7 +87,7 @@ function ready(error, sa1, boothdata, votes) {
       .rollup(function(d) { return d3.sum(d, function(d) { return d.votes; }); })
       .entries(d.votes);
 
-    d.votes = nest.map(function(d) { return [d.key, d.values]; });
+    d.votes = nest.map(function(d) { o = {}; o[d.key] = d.values; return o; });
   });
 
   var polygons = d3.geom.voronoi()
@@ -112,9 +114,8 @@ function ready(error, sa1, boothdata, votes) {
     //d.booths = d.booths.length;
   });
 
-  var centroidPoints = [];
-  geo.forEach(function(d) {
-    centroidPoints.push({id: d.id, position: d3.geo.centroid(d)});
+  var centroidPoints = geo.map(function(d) {
+    return {id: d.id, position: d3.geo.centroid(d)};
   });
 
   var centroidQuadtree = d3.geom.quadtree()
@@ -133,15 +134,58 @@ function ready(error, sa1, boothdata, votes) {
     });
   });
 
-  var voronoi = geojson();
+  var sa1Map = d3.map();
+  sa1data.forEach(function(d) {
+    sa1Map.set(d.region_id, d);
+    for (var i in d) { d[i] = +d[i]; }
+  });
 
+
+  result.values().forEach(function(result) {
+    var tracts = result.tracts;
+    var sa1s = tracts.map(function(d) { return sa1Map.get(d); });
+    result.demographics = d3.nest()
+      .rollup(function(d) {
+        return {
+          population:           d3.sum(d, function(d) { return +d.Tot_P_P; }),
+          migrants:             d3.sum(d, function(d) { return +d.Birthplace_Elsewhere_P; }),
+          other_lang:           d3.sum(d, function(d) { return +d.Lang_spoken_home_Oth_Lang_P; }),
+          age_18_34:            d3.sum(d, function(d) { return +d.Age_yr_18_P + d.Age_yr_19_P + d.Age_yr_20_P + d.Age_yr_21_P + d.Age_yr_22_P + d.Age_yr_23_P + d.Age_yr_24_P + d.Total_25_34_yr; }),
+          age_35_54:            d3.sum(d, function(d) { return +d.Total_35_44_yr + d.Total_45_54_yr; }),
+          age_55_ov:            d3.sum(d, function(d) { return +d.Total_55_64_yr + d.Total_65_74_yr + d.Total_75_84_yr + d.Total_85ov; }),
+          christians:           d3.sum(d, function(d) { return +d.Christianity_Tot_P; }),
+          non_religious:        d3.sum(d, function(d) { return +d.No_Religion_P; }),
+          //year_12_equivalent:   d3.sum(d, function(d) { return +d.P_Y12e_tot; }),
+          couple_childless:     d3.sum(d, function(d) { return +d.Tot_cple_fam_no_child; }),
+          couple_child:         d3.sum(d, function(d) { return +d.Tot_cple_fam_wth_chld; }),
+          single_child:         d3.sum(d, function(d) { return +d.Tot_One_parent_fam; }),
+          no_internet:          d3.sum(d, function(d) { return +d.No_IC_Total; }),
+          median_age:        ~~d3.mean(d, function(d) { return +d.Median_age_persons; }),
+          median_income:     ~~d3.mean(d, function(d) { return +d.Median_Tot_fam_inc_weekly; })
+        };
+      })
+      .entries(sa1s);
+  });
+
+  var voronoi = geojson();
   result.values().forEach(function(result) {
     voronoi.features.push(polygonFeature(result.id, {
       booths: result.booths,
+      votes: result.votes,
       tracts: result.tracts,
-      votes: result.votes
+      demographics: result.demographics,
     }, result.voronoi));
   });
+
+  function analyseVotesToPopulation () {
+    var stats = result.values().map(function(result) {
+      var votes = d3.sum(result.votes, function(d) { return d[1]; });
+      var population = d3.sum([result.demographics.age_18_34, result.demographics.age_35_54, result.demographics.age_55_ov]);
+      return {votes: votes, population: population, ratio: (population && votes) ? population/votes : 0};
+    });
+    console.log(d3.median(stats, function(d) { return d.ratio; }));
+  }
+  //analyseVotesToPopulation();
 
   fs.writeFile("data/result.json", JSON.stringify(result, null, 2));
   fs.writeFile("data/voronoi.json", JSON.stringify(voronoi, null, 2));
@@ -161,28 +205,10 @@ function polygonFeature (id, properties, coordinates) {
   };
 }
 
-function pointFeature (id, coordinates) {
-  return {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": coordinates
-    },
-    "properties": {},
-    "id": id
-  };
-}
-
 function geojson() {
   return {
     "type": "FeatureCollection",
     "features": [],
-    "crs": {
-      "type": "name",
-      "properties": {
-        "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-      }
-    }
   };
 }
 
